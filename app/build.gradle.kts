5e607696232a94fbc3c4ae9089245441925a5577
HanchenYe <yehanchen@starkye.com>
2025-10-09 22:00:01 -0700

    update gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,12 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.2")
 
     // Navigation（Fragment jump）
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     // WorkManager & Room
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.room:room-runtime:2.6.1")
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
